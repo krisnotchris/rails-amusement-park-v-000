3cb39f40e4c2259ce1ccd3311169e6d8055a1e15
krisnotchris <krishenderson@gmail.com>
2016-09-15 23:01:40 -0500

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+  before_action :logged_in?, only: [:show]
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)
+  end
+
+  def signin
+    @user = User.new
+  end
+
+  def show
+    if logged_in?
+      @user = User.find(params[:id])
+    else
+      redirect_to '/'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :height, :happiness, :nausea, :tickets, :password, :admin)
+  end
+
+end
